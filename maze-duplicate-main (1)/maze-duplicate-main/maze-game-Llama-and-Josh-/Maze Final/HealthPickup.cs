2d7eb5fb83e14f0371dff768524264f7d7178ac0
JTM1512 <166008219+JTM1512@users.noreply.github.com>
2024-11-11 19:10:12 +0200

    Add files via upload

---
+++ maze-duplicate-main (1)/maze-duplicate-main/maze-game-Llama-and-Josh-/Maze Final/HealthPickup.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maze_Final
+{
+    [Serializable]
+    public class HealthPickupTile : PickupTile
+    {
+        public HealthPickupTile(Position position) : base(position)
+        {
+        }
+
+        public override void ApplyEffect(CharacterTile character)
+        {
+            character._HP += 10;
+            if (character._HP > character._MaxHP)
+            {
+                character._HP = character._MaxHP;
+            }
+        }
+
+        public override char Display
+        {
+            get
+            {
+                return '+';
+            }
+        }
+    }
+}
